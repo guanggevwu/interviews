330047f2e6992e808976ebeec3c9c135a9761860
guanggevwu <guanggevwu@gmail.com>
2022-04-03 09:12:52 -0500

    correction

--- flatten.py
+++ flatten.py
@@ -1,10 +1,22 @@
-#flatten
+'''flatten'''
+
 def flatten_a_level(x):
     newx = []
     for i in x:
-        for j in i:
-            newx.append(j)
+        if type(i) is list:
+            for j in i:
+                newx.append(j)
+        else:
+            newx.append(i)
     return newx
-x = [[[[1]]],[[[2]]]]
-while type(x[0]) is list:
-    x = flatten_a_level(x)
+x = [1,[2],'ad', [[5]]]
+while True:
+    mark = 0
+    for i in x:
+        if type(i) is list:
+            x = flatten_a_level(x)
+            mark = 1
+            break
+    if not mark:
+        print(x)
+        break
